045265b5a5b89faee20b1c4fdc623f5b57cf5563
Junior-45 <ginelceralus@gmail.com>
2025-03-31 21:13:38 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,11 +43,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&gKFXTY4a#lUHch5Od0rOUL-Lsc2lrPAPs9ibRwp3cVDp9vwWCoc8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&xCFFyChK#57ddTclnV6K7fKq7BmOlnUTDNhdcEYgSrIaCclWWLu8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || ',' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SUDO: process.env.SUDO === undefined ? '50935623320' : process.env.SUDO,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
+SUDO: process.env.SUDO === undefined ? '50955544052' : process.env.SUDO,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
 ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
