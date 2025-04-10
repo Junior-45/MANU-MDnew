3b7914728c9e67c33ca3119b0756d98406cb67f4
Junior-45 <ginelceralus@gmail.com>
2025-04-09 23:53:46 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,8 +43,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&xCFFyChK#57ddTclnV6K7fKq7BmOlnUTDNhdcEYgSrIaCclWWLu8' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || 'a' ,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&ESE3FTgZ#xmahJDzhvP7ZU4UNloMc5b6CeK2xxGet60VFfWB5bU0' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || 'e' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '50955544052' : process.env.SUDO,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
